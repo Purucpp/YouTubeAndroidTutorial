db11d60f90791caad89a2eae4c8dae55723c340a
Purushottam Kumar <purushottam@192.168.1.7>
2025-06-18 06:46:10 +0530

    implementation done

--- app/src/main/java/com/yesandroid/youtubeandroidtutorial/MainActivity.kt
+++ app/src/main/java/com/yesandroid/youtubeandroidtutorial/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.yesandroid.testsdk.MySdk
 import com.yesandroid.youtubeandroidtutorial.ui.theme.YouTubeAndroidTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $name! SDK: ${MySdk.getGreeting()}",
         modifier = modifier
     )
 }
